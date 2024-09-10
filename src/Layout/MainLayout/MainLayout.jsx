de1cb2f80d1e7b100a714914198d3f0fa2a2261c
Showrab Paul <supto50showrab@gmail.com>
2024-09-10 14:44:18 -0700

    vercel speed insights added in mainlayout

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 const MainLayout = () => {
     return (
         <>
 
             <Outlet/>
-            
+            <SpeedInsights/>
+            <Analytics/>
         </>
     );
 };
