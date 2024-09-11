32325a45cab504b29f7ea1510405f5dafba75032
Showrab Paul <supto50showrab@gmail.com>
2024-09-11 01:26:56 -0700

    lazy loader added

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -1,15 +1,37 @@
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/react"
-const MainLayout = () => {
-    return (
-        <>
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
-            <Outlet/>
-            <SpeedInsights/>
-            <Analytics/>
+const MainLayout = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Simulate a loading delay
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="flex items-center justify-center min-h-screen bg-brown-50">
+          <div className="relative flex items-center justify-center">
+            <div className="pulse-loader relative rounded-full border-8 border-t-brown-600 h-32 w-32 animate-spin"></div>
+            <div className="absolute text-brown-700 font-bold text-lg mt-40 animate-pulse">Loading...</div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Outlet />
+          <SpeedInsights />
+          <Analytics />
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default MainLayout;
